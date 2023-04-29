f73526efde976140883927682709bca7e1853af2
jsweet-dev <sweet.jonathan@gmail.com>
2023-04-29 01:14:46 -0700

    New route added /reports/detailed for detailed PDF report generation

--- backend/api.js
+++ backend/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {getMinerStatistics, getOutages } = require('./dbFunctions');
+const { generatePdfWrapper } = require('./reportFunctions');
 
 // api.js
 router.post('/workers', async (req, res) => {
@@ -28,6 +29,27 @@ router.post('/outages', async (req, res) => {
     res.status(500).json({ message: 'Error retrieving outages', error: error });
   }
 });
+
+router.post('/reports/detailed', async (req, res) => {
+  try {
+    console.log("Post to /reports/detailedReport")
+    const  { searchTerm } = req.body;
+
+    const startTime = new Date(searchTerm.dateRange.startDate).getTime();
+    const endTime = new Date(searchTerm.dateRange.endDate).getTime();
+
+    const outages = await getOutages(startTime, endTime);
+    console.log("Outages: ", outages.length);
+    const report = await generatePdfWrapper(outages, searchTerm);
+    // setTimeout(() => {
+    //   res.json("#Success"+JSON.stringify(outages.length));
+    // }, 10000);
+    res.status(200).send({ report });
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving detailed report', error: error });
+  }
+});
+
 
 
 // router.post('/worker/:workerId/stats', async (req, res) => {
