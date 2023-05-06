3e2102bf6e1ef89706641277f370510f40e43bff
jsweet-dev <sweet.jonathan@gmail.com>
2023-05-05 20:10:03 -0700

    Changed default date range to current month start and end

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -10,24 +10,27 @@ import {
   MenuItem,
   Popover
 } from '@mui/material';
+import { startOfMonth, endOfMonth } from 'date-fns';
 import { DateRangePicker } from 'react-date-range';
-import 'react-date-range/dist/styles.css'; 
-import 'react-date-range/dist/theme/default.css'; 
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 
 const SearchBar = ({ filterCriteria, onSearch }) => {
   const [anchorEl, setAnchorEl] = useState(null);
 
   const defaultSearchQuery = useMemo(() => {
-    const currentTime = new Date().getTime();
+    const calculatedStartOfMonth = startOfMonth(new Date()).getTime()
+    const calculatedEndOfMonth = endOfMonth(new Date()).getTime()
+
     const query = {};
     filterCriteria.forEach((criteria) => {
       const key = Object.keys(criteria)[0];
-      if(criteria[key][0] === 'dateRange'){
-        query[key] = { 
-          startDate: currentTime - 30 * 24 * 60 * 60 * 1000, 
-          endDate: currentTime
-         };
+      if (criteria[key][0] === 'dateRange') {
+        query[key] = {
+          startDate: calculatedStartOfMonth,
+          endDate: calculatedEndOfMonth
+        };
       } else {
         query[key] = '';
       }
@@ -58,11 +61,10 @@ const SearchBar = ({ filterCriteria, onSearch }) => {
   };
 
   const formatDate = (date) => {
-    if(!date) return '';
+    if (!date) return '';
     date = new Date(date);
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
   };
-  
 
   const handleClose = () => {
     setAnchorEl(null);
@@ -90,112 +92,112 @@ const SearchBar = ({ filterCriteria, onSearch }) => {
   };
 
   return (
-      <Box my={4}>
-        <Grid container spacing={2}>
-          {filterCriteria.map((criteria, index) => {
-            const key = Object.keys(criteria)[0];
-            const [type, customLabels] = criteria[key];
+    <Box my={4}>
+      <Grid container spacing={2}>
+        {filterCriteria.map((criteria, index) => {
+          const key = Object.keys(criteria)[0];
+          const [type, customLabels] = criteria[key];
 
-            if (type === 'bool') {
-              return (
-                <Grid item xs={12} sm lg xl key={index}>
-                  <FormControl fullWidth>
-                    <InputLabel>{getLabel(key, customLabels)}</InputLabel>
-                    <Select
-                      name={key}
-                      value={searchQuery[key]}
-                      onChange={handleChange}
-                      onKeyPress={handleKeyPress}
-                      label={getLabel(key, customLabels)}
-                    >
-                      <MenuItem value="">
-                        <em>Any</em>
-                      </MenuItem>
-                      <MenuItem value="true">{getLabel('true', customLabels)}</MenuItem>
-                      <MenuItem value="false">{getLabel('false', customLabels)}</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-              );
-            } else if (type === 'dateRange') {
-              const open = Boolean(anchorEl);
-              const id = open ? 'simple-popover' : undefined;
-            
-              return (
-                <Grid item xs={12} sm lg xl key={index}>
-                  <TextField
-                    fullWidth={true}
-                    label="Date Range"
-                    onClick={handleClick}
-                    value={`${formatDate(searchQuery[key].startDate)} ~ ${formatDate(searchQuery[key].endDate)}`}
-                  />
-                  <Popover
-                    id={id}
-                    open={open}
-                    anchorEl={anchorEl}
-                    onClose={handleClose}
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'left',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'left',
-                    }}
-                  >
-                    <DateRangePicker
-                      onChange={(item) => {
-                        handleDateChange(
-                          key,
-                          item.range1.startDate,
-                          item.range1.endDate
-                        );
-                        handleClose();
-                      }}
-                      showSelectionPreview={true}
-                      moveRangeOnFirstSelection={false}
-                      months={2}
-                      direction="horizontal"
-                      ranges={[
-                        {
-                          startDate: searchQuery[key].startDate,
-                          endDate: searchQuery[key].endDate,
-                          key: 'range1',
-                        },
-                      ]}
-                      rangeColors={['#3f51b5']}
-                    />
-                  </Popover>
-                </Grid>
-              );
-            }
-            
-            else {
-              return (
-                <Grid item xs={12} sm lg xl  key={index}>
-                  <TextField
-                    label={getLabel(key, customLabels)}
+          if (type === 'bool') {
+            return (
+              <Grid item xs={12} sm lg xl key={index}>
+                <FormControl fullWidth>
+                  <InputLabel>{getLabel(key, customLabels)}</InputLabel>
+                  <Select
                     name={key}
                     value={searchQuery[key]}
                     onChange={handleChange}
                     onKeyPress={handleKeyPress}
-                    fullWidth
+                    label={getLabel(key, customLabels)}
+                  >
+                    <MenuItem value="">
+                      <em>Any</em>
+                    </MenuItem>
+                    <MenuItem value="true">{getLabel('true', customLabels)}</MenuItem>
+                    <MenuItem value="false">{getLabel('false', customLabels)}</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            );
+          } else if (type === 'dateRange') {
+            const open = Boolean(anchorEl);
+            const id = open ? 'simple-popover' : undefined;
+
+            return (
+              <Grid item xs={12} sm lg xl key={index}>
+                <TextField
+                  fullWidth={true}
+                  label="Date Range"
+                  onClick={handleClick}
+                  value={`${formatDate(searchQuery[key].startDate)} ~ ${formatDate(searchQuery[key].endDate)}`}
+                />
+                <Popover
+                  id={id}
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                >
+                  <DateRangePicker
+                    onChange={(item) => {
+                      handleDateChange(
+                        key,
+                        item.range1.startDate,
+                        item.range1.endDate
+                      );
+                      handleClose();
+                    }}
+                    showSelectionPreview={true}
+                    moveRangeOnFirstSelection={false}
+                    months={2}
+                    direction="horizontal"
+                    ranges={[
+                      {
+                        startDate: searchQuery[key].startDate,
+                        endDate: searchQuery[key].endDate,
+                        key: 'range1',
+                      },
+                    ]}
+                    rangeColors={['#3f51b5']}
                   />
-                </Grid>
-              );
-            }
-          })}
-          <Grid item xs={12} sx={{'& > button': { marginRight: '15px'}}}>
-            <Button variant="contained" color="primary" onClick={handleSubmit}>
-              Search
-            </Button>
-            <Button variant="contained" color="secondary" onClick={() => clearSearch()}>
-              Clear
-            </Button>
-          </Grid>
+                </Popover>
+              </Grid>
+            );
+          }
+
+          else {
+            return (
+              <Grid item xs={12} sm lg xl key={index}>
+                <TextField
+                  label={getLabel(key, customLabels)}
+                  name={key}
+                  value={searchQuery[key]}
+                  onChange={handleChange}
+                  onKeyPress={handleKeyPress}
+                  fullWidth
+                />
+              </Grid>
+            );
+          }
+        })}
+        <Grid item xs={12} sx={{ '& > button': { marginRight: '15px' } }}>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Search
+          </Button>
+          <Button variant="contained" color="secondary" onClick={() => clearSearch()}>
+            Clear
+          </Button>
         </Grid>
-      </Box>
-  );    
+      </Grid>
+    </Box>
+  );
 };
 
 export default SearchBar;
